2cd8670289f0f1268635c442dcefa6df94047edb
cheecko <wewsa1@live.com>
2021-10-15 16:24:28 +0200

    fix error

--- index.js
+++ index.js
@@ -4,29 +4,26 @@ let app = express();
 let bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.get('/', function(req, res) {
   res.send('Hello World');
+})
 
-  app.get('/', function(req, res) {
-      res.send('Hello World');
-  })
+app.get('/api/v1/users/:userId', (req, res) => {
+  var users = {}
+  let randomuserid = uuidv4()
+  let user = {
+      vorname: "Anne",
+      nachname: "Musterfrau",
+      userid: randomuserid
+  }
+  users[randomuserid] = user;
+  let myuser = users[randomuserid];
+  return res.send(myuser);
+});
 
-  app.get('/api/v1/users/:userId', (req, res) => {
-      var users = {}
-      let randomuserid = uuidv4()
-      let user = {
-          vorname: "Anne",
-          nachname: "Musterfrau",
-          userid: randomuserid
-      }
-      users[randomuserid] = user;
-      let myuser = users[randomuserid];
-      return res.send(myuser);
-  });
-
-  let server = app.listen(8080, function() {
-      var host = server.address().address
-      var port = server.address().port
-      console.log("Webserver running at http://%s:%s", host,
-          port)
-  })
+let server = app.listen(8080, function() {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Webserver running at http://%s:%s", host, port)
+})

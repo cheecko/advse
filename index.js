71c7925ed4d6ab5c9d548e7af6c8d6aa8c735764
cheecko <wewsa1@live.com>
2021-10-15 15:31:59 +0200

    other command

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let app = express();
 let bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.get('/', function (req, res) {
 res.send('Hello World');
 })
